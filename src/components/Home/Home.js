de5763999485038749cbea9aa22c79e8baf8af3a
Trevbo25 <trevorbrown25@gmail.com>
2017-10-11 15:24:13 -0700

    didnt break the home page adding another scroll

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -36,7 +36,7 @@ class Home extends Component{
 
     handleScroll(e){
         let newScroll = this.state.scroll;
-        e.deltaY > 0 ? ( this.state.scroll < 2 ? this.setState({
+        e.deltaY > 0 ? ( this.state.scroll < 3 ? this.setState({
             scroll: newScroll + 1
         }) : null) :
         (this.state.scroll > 0 ? this.setState({
@@ -44,14 +44,14 @@ class Home extends Component{
         }) : null)
     }
 
+    //<div className={this.state.scroll === 1 ? "papah papahgo" : "papah"}>
 
     render(){
         console.log(this.state.scroll);
         return(
                 <div onWheel={e => {this.handleScroll(e); e.persist();}}>
-                    <div className={this.state.scroll === 1 ? "papah papahgo" : "papah"}>
-                        <div className="navbar">
-                            <a href="/#/home"><div className="navhome"><h1>G</h1></div></a>
+                    <div className="navbar">
+                            <a href="/#/home"><div className="navhome"><h1 className="g">G</h1></div></a>
                             <div className="navlinks">
                                 <a href="/#/out"><div>Eat Out</div></a>
                                 <a href="/#/in"><div>Eat In</div></a>
@@ -62,47 +62,26 @@ class Home extends Component{
                                 <a href="http://localhost:3535/auth/logout"><div>Logout</div></a>
                             </div>
                         </div>
+                    <div className={this.state.scroll === 1 ? "topcardshow topcardgo" : "topcardshow"}>       
+                    </div>
+                    <div className={this.state.scroll === 2 ? "papah papahgo" : ( this.state.scroll === 1 ? "papah" : "papahhide")}>
                         <div className="topholder">
                             <div className="toptext">
-                                <h4>Hey. We know you're hungry but you can't decide what you want to eat. It's tough. We've all been there and we're here to help with that. Making such an impossible decision on an empty stomach is something nobody should have to suffer through alone. Now you don't have to. Help us help you, scroll down to find out your options.</h4>
+                                <h4>Hey. It's okay. We know you can't decide what you want to eat. It's tough. We've all been there and we're here to help with that. Making such an impossible decision on an empty stomach is something nobody should have to suffer through alone. Now you don't have to. Help us help you.</h4>
                             </div>
                         </div>
                     </div>
-                        <div className={this.state.scroll === 1 ? "maincardshow" : ( this.state.scroll === 2 ? "maincardgo" : "maincard")}>
-                            <div className="navbar">
-                                <a href="/#/home"><div className="navhome"><h1>G</h1></div></a>
-                                <div className="navlinks">
-                                    <a href="/#/out"><div>Eat Out</div></a>
-                                    <a href="/#/in"><div>Eat In</div></a>
-                                    <a href="/#/favorites"><div>Favorites</div></a>
-                                </div>
-                                <div className="navlinksright">
-                                    <a href="/#/about"><div>About</div></a>
-                                    <a href="http://localhost:3535/auth/logout"><div>Logout</div></a>
-                                </div>
-                            </div>
-                                <div className="maincardcontent">
-                                    <div className="mainwords">
-                                        <h1 className="maincardtitle">Feeling ambitious?</h1>
-                                        <br/>
-                                        <h4>Really? Well then, I have just the thing for you. We can help you whip something up with step by step directions. Why don't you try your hand at making eggs benedict, a juicy New York strip, or a spicy bloody mary. Thousands of recipes are at your fingertips. With a couple clicks and a little elbow grease you'll go from get me fed to me got fed.</h4><br/>
-                                        <a href="/#/in"><div className="inbutton"><p>Let's eat in!</p><div className="arrow"></div></div></a>
-                                    </div>
-                                </div>
-                            </div>
-                        <div className={this.state.scroll === 2 ? "secondcardshow" : "secondcard"}>
-                        <div className="navbar">
-                            <a href="/#/home"><div className="navhome"><h1>G</h1></div></a>
-                            <div className="navlinks">
-                                <a href="/#/out"><div>Eat Out</div></a>
-                                <a href="/#/in"><div>Eat In</div></a>
-                                <a href="/#/favorites"><div>Favorites</div></a>
-                            </div>
-                            <div className="navlinksright">
-                                <a href="/#/about"><div>About</div></a>
-                                <a href="http://localhost:3535/auth/logout"><div>Logout</div></a>
+                    <div className={this.state.scroll === 2 ? "maincardshow" : ( this.state.scroll === 3 ? "maincardgo" : "maincard")}>
+                        <div className="maincardcontent">
+                            <div className="mainwords">
+                                <h1 className="maincardtitle">Feeling ambitious?</h1>
+                                <br/>
+                                <h4>Really? Well then, I have just the thing for you. We can help you whip something up with step by step directions. Why don't you try your hand at making eggs benedict, a juicy New York strip, or a spicy bloody mary. Thousands of recipes are at your fingertips. With a couple clicks and a little elbow grease you'll go from get me fed to me got fed.</h4><br/>
+                                <a href="/#/in"><div className="inbutton"><p>Let's eat in!</p><div className="arrow"></div></div></a>
                             </div>
                         </div>
+                    </div>
+                    <div className={this.state.scroll === 3 ? "secondcardshow" : "secondcard"}>
                         <div className="secondcardcontent">
                             <div className="secondwords">
                                 <h1 className="secondcardtitle">The effort not there today?</h1><br/>
@@ -110,7 +89,6 @@ class Home extends Component{
                                 <a href="/#/out"><div className="outbutton"><div className="arrow"></div><p>Let's eat out!</p></div></a>
                             </div>
                         </div>
-                        
                     </div>
                 </div>
         )
